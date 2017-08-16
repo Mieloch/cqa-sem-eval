663bbfa779ac06bc5dd3174e2511d75268e959d5
Paweł Mieloch <pawel.mieloch@gmail.com>
2017-08-16 21:44:19 +0200

    question to related comments statistics

--- basic_stats.py
+++ basic_stats.py
@@ -1,21 +1,6 @@
 from bs4 import BeautifulSoup
 import re
 import nltk
-import csv
-import spacy
-
-
-RELATED_QUESTION_ID = "related_question_id"
-
-ORGINAL_QUESTION_ID = "original_id"
-
-RELEVANCE = "relevance"
-
-LENGTH_DIFFERENCE = "length_difference"
-
-JACCARD_DISTANCE = "jaccard_distance"
-
-COSINE_SIMILARITY = "cosine_similarity"
 
 
 def load(file_name):
@@ -37,32 +22,8 @@ def jaccard_distance(original, related):
 
     return nltk.jaccard_distance(org_tokens, rel_tokens)
 
+
 def cosine_similarity(model, original, related):
     org_tokens = model(original)
     rel_tokens = model(related)
     return org_tokens.similarity(rel_tokens)
-
-model = spacy.load('en')
-
-soup = load('SemEval2016-Task3-CQA-QL-train-part1.xml')
-original_questions = soup.findAll("OrgQuestion")
-
-with open('train_set_stat_part1.csv', 'w') as csvfile:
-    fieldnames = [ORGINAL_QUESTION_ID, RELATED_QUESTION_ID, JACCARD_DISTANCE, LENGTH_DIFFERENCE, COSINE_SIMILARITY, RELEVANCE]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, lineterminator='\n')
-    writer.writeheader()
-
-    for original_question in original_questions:
-        related_questions = original_question.findAll("RelQuestion")
-        for related_question in related_questions:
-            row = {}
-            related_question_body = remove_subject_from_question(related_question.RelQBody.text)
-
-            orginal_question_body = remove_subject_from_question(original_question.OrgQBody.text)
-            row[ORGINAL_QUESTION_ID] = original_question['ORGQ_ID']
-            row[RELATED_QUESTION_ID] = related_question['RELQ_ID']
-            row[JACCARD_DISTANCE] = round(jaccard_distance(orginal_question_body, related_question_body), 3)
-            row[LENGTH_DIFFERENCE] = length_difference(orginal_question_body, related_question_body)
-            row[COSINE_SIMILARITY] = round(cosine_similarity(model, orginal_question_body, related_question_body), 3)
-            row[RELEVANCE] = related_question['RELQ_RELEVANCE2ORGQ']
-            writer.writerow(row)

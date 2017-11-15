b362edf18874cb18240f91c3b378497ceaf6d8af
Paweł Mieloch <pawel.mieloch@gmail.com>
2017-11-15 13:05:32 +0100

    add util function to load different word2vec models

--- word2vec_model/word2vec_utils.py
+++ word2vec_model/word2vec_utils.py
@@ -1,9 +1,25 @@
 import nltk
 import numpy as np
-
+import gensim
 import sklearn
 import math
+import os
 from stop_words import get_stop_words
+
+
+def load_word2vec_model(model_name):
+    print("Loading word2vec model {}".format(model_name))
+    path = os.path.dirname(os.path.abspath(__file__))
+    model = None
+    if model_name == "Q1_model" or model_name == "SemEval2016-Task3-CQA-QL-dev_model":
+        model = gensim.models.Word2Vec.load(path + "/" + model_name)
+    elif model_name == "GoogleNews-vectors-negative300.bin":
+        model = gensim.models.KeyedVectors.load_word2vec_format(path + "/" + model_name,
+                                                                binary=True)
+    else:
+        raise Exception("Unknown word2vec model {}".format(model_name))
+    print("Loading word2vec model {} [DONE]".format(model_name))
+    return model
 
 
 def vectors_cosine_similarity(vector1, vector2):

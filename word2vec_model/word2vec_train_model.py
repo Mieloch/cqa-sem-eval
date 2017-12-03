f4a54915d5df3f02fc4b4f8fbe632c39f7fd3c78
Tomasz Kasperek <kasperekt@gmail.com>
2017-12-03 22:00:41 +0100

    WIP: modify scripts for subtask E

--- word2vec_model/word2vec_train_model.py
+++ word2vec_model/word2vec_train_model.py
@@ -1,58 +1,45 @@
 import gensim
-import argparse
 import basic_stats
 from word2vec_utils import tokenize_to_lower_case
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--subtask-e',
-                    dest='subtask_e',
-                    help='Prepare word embeddings for subtask E (different dataset)',
-                    action='store_true')
 
-args = parser.parse_args()
+def train_model(data_src, model_name, verbose=False):
+    soup = basic_stats.load(data_src)
+    original_questions = soup.findAll("OrgQuestion")
 
-if args.subtask_e:
-    soup = basic_stats.load('../data/subtask_e_sample.xml')
-else:
-    soup = basic_stats.load('../data/Q1_sample.xml')
+    sentences = []
+    processed_ids = []
+    tokens_count = 0
 
-original_questions = soup.findAll("OrgQuestion")
+    def parse_original_question(question_element):
+        id = original_question["ORGQ_ID"]
+        if id not in processed_ids:
+            processed_ids.append(id)
+            original_question_body = basic_stats.remove_subject_from_question(
+                original_question.OrgQBody.text
+            )
+            tokenize = tokenize_to_lower_case(original_question_body)
+            tokens_count += len(tokenize)
+            sentences.append(tokenize)
 
-sentences = []
-processed_ids = []
-tokens_count = 0
-# creating corpus
-for original_question in original_questions:
-    # original questions
-    id = original_question["ORGQ_ID"]
-    if id not in processed_ids:
-        processed_ids.append(id)
-        original_question_body = basic_stats.remove_subject_from_question(
-            original_question.OrgQBody.text
-        )
-        tokenize = tokenize_to_lower_case(original_question_body)
-        tokens_count += len(tokenize)
-        sentences.append(tokenize)
+        # related questions
+        related_questions = original_question.findAll("RelQuestion")
+        for related_question in related_questions:
+            related_question_body = basic_stats.remove_subject_from_question(
+                related_question.RelQBody.text)
+            tokenize = tokenize_to_lower_case(related_question_body)
+            tokens_count += len(tokenize)
+            sentences.append(tokenize)
 
-    # related questions
-    related_questions = original_question.findAll("RelQuestion")
-    for related_question in related_questions:
-        related_question_body = basic_stats.remove_subject_from_question(
-            related_question.RelQBody.text)
-        tokenize = tokenize_to_lower_case(related_question_body)
-        tokens_count += len(tokenize)
-        sentences.append(tokenize)
+        # related comments
+        related_comments = original_question.findAll("RelComment")
+        for related_comment in related_comments:
+            tokenize = tokenize_to_lower_case(related_comment.RelCText.text)
+            # tokenize = tokenize_to_lower_case(related_comment.RelCClean.text)
+            tokens_count += len(tokenize)
+            sentences.append(tokenize)
 
-    # related comments
-    related_comments = original_question.findAll("RelComment")
-    for related_comment in related_comments:
-        tokenize = tokenize_to_lower_case(related_comment.RelCText.text)
-        # tokenize = tokenize_to_lower_case(related_comment.RelCClean.text)
-        tokens_count += len(tokenize)
-        sentences.append(tokenize)
-
-    # related answers (only for subtask e)
-    if args.subtask_e:
+        # related answers (only for subtask e)
         related_answers = original_question.findAll("RelAnswer")
         for related_answer in related_answers:
             body = related_answer.RelAText.text
@@ -60,8 +47,34 @@ for original_question in original_questions:
             tokens_count += len(tokenize)
             sentences.append(tokenize)
 
+    # creating corpus
+    for original_question in original_questions:
+        # original questions
+        parse_original_question(original_question)
 
-print(tokens_count)
-# print(sentences)
-model = gensim.models.Word2Vec(sentences, min_count=1, window=3, iter=1000, size=100)
-model.save('Q1_model')
+    if verbose:
+        print("Token count: {}".format(tokens_count))
+
+    model = gensim.models.Word2Vec(
+        sentences, min_count=1, window=3, iter=1000, size=100)
+    model.save(model_name)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data',
+                        dest='data',
+                        help='Training data path (default = data/Q1_sample.xml)',
+                        default='../data/Q1_sample.xml')
+    parser.add_argument('--model-name',
+                        dest='model_name',
+                        help='Model file name',
+                        default='Q1_model')
+    parser.add_argument('--verbose',
+                        dest='verbose',
+                        action='store_true')
+    args = parser.parse_args()
+
+    train_model(args.data, args.model_name, verbose=args.verbose)

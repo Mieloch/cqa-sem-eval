6b5c1617152cdf9a057fbfcdfcc14b3a0f5e8d2d
Tomasz Kasperek <kasperekt@gmail.com>
2017-12-05 17:38:36 +0100

    Modify word2vec training script to use iterparse

--- word2vec_model/word2vec_train_model.py
+++ word2vec_model/word2vec_train_model.py
@@ -1,60 +1,64 @@
 import gensim
 import basic_stats
 from word2vec_utils import tokenize_to_lower_case
+from lxml import etree
+
+class Sentences(object):
+    def __init__(self, xml_path, verbose=False):
+        self.xml_path = xml_path
+        self.tokens_count = 0
+        self.verbose = verbose
+
+    def __iter__(self):
+        processed_ids = []
+        self.tokens_count = 0
+
+        for event, original_question in etree.iterparse(self.xml_path, tag="OrgQuestion"):
+            id = original_question.get("ORGQ_ID")
+
+            if id not in processed_ids:
+                processed_ids.append(id)
+                original_question_body = original_question.findtext("OrgQBody")
+                original_question_body = basic_stats.remove_subject_from_question(
+                    original_question_body
+                )
+                tokenize = tokenize_to_lower_case(original_question_body)
+                self.tokens_count += len(tokenize)
+                yield tokenize
+
+            # related questions
+            related_questions = original_question.findall(".//RelQuestion")
+            for related_question in related_questions:
+                related_question_body = related_question.findtext('RelQBody')
+                related_question_body = basic_stats.remove_subject_from_question(
+                    related_question_body)
+                tokenize = tokenize_to_lower_case(related_question_body)
+                self.tokens_count += len(tokenize)
+                yield tokenize
+
+            # related comments
+            related_comments = original_question.findall(".//RelComment")
+            for related_comment in related_comments:
+                related_comment_body = related_comment.findtext('RelCText')
+                tokenize = tokenize_to_lower_case(related_comment_body)
+                # tokenize = tokenize_to_lower_case(related_comment.RelCClean.text)
+                self.tokens_count += len(tokenize)
+                yield tokenize
+
+            # related answers (only for subtask e)
+            related_answers = original_question.findall(".//RelAnswer")
+            for related_answer in related_answers:
+                related_answer_body = related_answer.findtext('RelAText')
+                tokenize = tokenize_to_lower_case(related_answer_body)
+                self.tokens_count += len(tokenize)
+                yield tokenize
+
+            if self.verbose:
+                print("Tokens count = {}".format(self.tokens_count))
 
 
 def train_model(data_src, model_name, verbose=False):
-    soup = basic_stats.load(data_src)
-    original_questions = soup.findAll("OrgQuestion")
-
-    sentences = []
-    processed_ids = []
-    tokens_count = 0
-
-    def parse_original_question(question_element):
-        id = original_question["ORGQ_ID"]
-        if id not in processed_ids:
-            processed_ids.append(id)
-            original_question_body = basic_stats.remove_subject_from_question(
-                original_question.OrgQBody.text
-            )
-            tokenize = tokenize_to_lower_case(original_question_body)
-            tokens_count += len(tokenize)
-            sentences.append(tokenize)
-
-        # related questions
-        related_questions = original_question.findAll("RelQuestion")
-        for related_question in related_questions:
-            related_question_body = basic_stats.remove_subject_from_question(
-                related_question.RelQBody.text)
-            tokenize = tokenize_to_lower_case(related_question_body)
-            tokens_count += len(tokenize)
-            sentences.append(tokenize)
-
-        # related comments
-        related_comments = original_question.findAll("RelComment")
-        for related_comment in related_comments:
-            tokenize = tokenize_to_lower_case(related_comment.RelCText.text)
-            # tokenize = tokenize_to_lower_case(related_comment.RelCClean.text)
-            tokens_count += len(tokenize)
-            sentences.append(tokenize)
-
-        # related answers (only for subtask e)
-        related_answers = original_question.findAll("RelAnswer")
-        for related_answer in related_answers:
-            body = related_answer.RelAText.text
-            tokenize = tokenize_to_lower_case(body)
-            tokens_count += len(tokenize)
-            sentences.append(tokenize)
-
-    # creating corpus
-    for original_question in original_questions:
-        # original questions
-        parse_original_question(original_question)
-
-    if verbose:
-        print("Token count: {}".format(tokens_count))
-
+    sentences = Sentences(data_src, verbose=verbose)
     model = gensim.models.Word2Vec(
         sentences, min_count=1, window=3, iter=1000, size=100)
     model.save(model_name)

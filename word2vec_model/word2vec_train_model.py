9c0fd4339cf8a743544986c11826247d80a47c96
Tomasz Kasperek <kasperekt@gmail.com>
2017-12-13 15:21:42 +0100

    Clear element after it is parsed

--- word2vec_model/word2vec_train_model.py
+++ word2vec_model/word2vec_train_model.py
@@ -2,6 +2,8 @@ import gensim
 import basic_stats
 from word2vec_utils import tokenize_to_lower_case
 from lxml import etree
+import os
+
 
 class Sentences(object):
     def __init__(self, xml_path, verbose=False):
@@ -13,54 +15,62 @@ class Sentences(object):
         processed_ids = []
         self.tokens_count = 0
 
-        for event, original_question in etree.iterparse(self.xml_path, tag="OrgQuestion"):
-            id = original_question.get("ORGQ_ID")
+        file_size = os.path.getsize(self.xml_path)
+        with open(self.xml_path, mode='rb') as fp:
+            for event, original_question in etree.iterparse(fp, tag="OrgQuestion"):
+                id = original_question.get("ORGQ_ID")
 
-            if id not in processed_ids:
-                processed_ids.append(id)
-                original_question_body = original_question.findtext("OrgQBody")
-                original_question_body = basic_stats.remove_subject_from_question(
-                    original_question_body
-                )
-                tokenize = tokenize_to_lower_case(original_question_body)
-                self.tokens_count += len(tokenize)
-                yield tokenize
+                if id not in processed_ids:
+                    processed_ids.append(id)
+                    original_question_body = original_question.findtext(
+                        "OrgQBody")
+                    original_question_body = basic_stats.remove_subject_from_question(
+                        original_question_body
+                    )
+                    tokenize = tokenize_to_lower_case(original_question_body)
+                    self.tokens_count += len(tokenize)
+                    yield tokenize
 
-            # related questions
-            related_questions = original_question.findall(".//RelQuestion")
-            for related_question in related_questions:
-                related_question_body = related_question.findtext('RelQBody')
-                related_question_body = basic_stats.remove_subject_from_question(
-                    related_question_body)
-                tokenize = tokenize_to_lower_case(related_question_body)
-                self.tokens_count += len(tokenize)
-                yield tokenize
+                # related questions
+                related_questions = original_question.findall(".//RelQuestion")
+                for related_question in related_questions:
+                    related_question_body = related_question.findtext(
+                        'RelQBody')
+                    related_question_body = basic_stats.remove_subject_from_question(
+                        related_question_body)
+                    tokenize = tokenize_to_lower_case(related_question_body)
+                    self.tokens_count += len(tokenize)
+                    yield tokenize
 
-            # related comments
-            related_comments = original_question.findall(".//RelComment")
-            for related_comment in related_comments:
-                related_comment_body = related_comment.findtext('RelCText')
-                tokenize = tokenize_to_lower_case(related_comment_body)
-                # tokenize = tokenize_to_lower_case(related_comment.RelCClean.text)
-                self.tokens_count += len(tokenize)
-                yield tokenize
+                # related comments
+                related_comments = original_question.findall(".//RelComment")
+                for related_comment in related_comments:
+                    related_comment_body = related_comment.findtext('RelCText')
+                    tokenize = tokenize_to_lower_case(related_comment_body)
+                    self.tokens_count += len(tokenize)
+                    yield tokenize
 
-            # related answers (only for subtask e)
-            related_answers = original_question.findall(".//RelAnswer")
-            for related_answer in related_answers:
-                related_answer_body = related_answer.findtext('RelAText')
-                tokenize = tokenize_to_lower_case(related_answer_body)
-                self.tokens_count += len(tokenize)
-                yield tokenize
+                # related answers (only for subtask e)
+                related_answers = original_question.findall(".//RelAnswer")
+                for related_answer in related_answers:
+                    related_answer_body = related_answer.findtext('RelAText')
+                    tokenize = tokenize_to_lower_case(related_answer_body)
+                    self.tokens_count += len(tokenize)
+                    yield tokenize
 
-            if self.verbose:
-                print("Tokens count = {}".format(self.tokens_count))
+                if self.verbose:
+                    print("Tokens = {}, Progress = {}".format(
+                        self.tokens_count,
+                        float(fp.tell()) / file_size), end='\r')
+
+
+                original_question.clear()
 
 
 def train_model(data_src, model_name, verbose=False):
     sentences = Sentences(data_src, verbose=verbose)
     model = gensim.models.Word2Vec(
-        sentences, min_count=1, window=3, iter=1000, size=100)
+        sentences, min_count=5, window=5, iter=10, size=100, workers=4)
     model.save(model_name)
 
 

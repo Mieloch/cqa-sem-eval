3326a25655b656529b12ca3e4a467440eacb1ef2
Tomasz Kasperek <kasperekt@gmail.com>
2017-12-20 19:33:55 +0100

    Update stat scripts

--- subtask_E/make_stats.py
+++ subtask_E/make_stats.py
@@ -9,29 +9,32 @@ def main(verbose):
 
     data_files = [
         data_files_dir + '/stackexchange_english_train_v1_2/stackexchange_english_train.xml',
+        data_files_dir + '/stackexchange_english_train_v1_2/stackexchange_english_devel.xml',
         # data_files_dir + '/stackexchange_android_train_v1_2/stackexchange_android_train.xml',
         # data_files_dir + '/stackexchange_wordpress_train_v1_2/stackexchange_wordpress_train.xml',
         # data_files_dir + '/stackexchange_gaming_train_v1_2/stackexchange_gaming_train.xml',
     ]
 
     model_files = [
+        # '/Volumes/DataDrive/models/GoogleNews-vectors-negative300.bin',
+        model_files_dir + '/stackexchange_english_devel.xml--it50-mc5-s200.mdl',
+        model_files_dir + '/stackexchange_english_devel.xml--it50-mc5-s100.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it10-mc5-s100.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it30-mc5-s100.mdl',
-        # model_files_dir + '/stackexchange_english_devel.xml--it50-mc5-s100.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it10-mc10-s100.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it30-mc10-s100.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it50-mc10-s100.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it10-mc5-s200.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it30-mc5-s200.mdl',
-        model_files_dir + '/stackexchange_english_devel.xml--it50-mc5-s200.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it10-mc10-s200.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it30-mc10-s200.mdl',
         # model_files_dir + '/stackexchange_english_devel.xml--it50-mc10-s200.mdl',
     ]
 
     for data_file, model_file in product(data_files, model_files):
-        dest_file_name = "{}--{}.csv".format(path.basename(data_file),
-                                             path.basename(model_file))
+        dest_file_name = "/Volumes/DataDrive/stats/{}--{}.csv".format(
+            path.basename(data_file),
+            path.basename(model_file))
 
         if verbose:
             print('Writing to {}...'.format(dest_file_name))
--- subtask_E/stats.py
+++ subtask_E/stats.py
@@ -11,32 +11,31 @@ from lxml import etree
 from os import path
 
 ORIGINAL_QUESTION_ID = "question_id"
-TYPE = 'type'
-RELATED_ID = "related_id"
+RELATED_QUESTION_ID = "related_question_id"
+CATEGORY = "category"
+SCORE = "score"
+TAGS = "tags"
+VIEW_COUNT = "view_count"
+USER_ID = "user_id"
 RELEVANCE = "relevance"
+
 LENGTH_DIFFERENCE = "length_difference"
 JACCARD_DISTANCE = "jaccard_distance"
 COSINE_SIMILARITY = "cosine_similarity"
 BIGRAM_SIMILARITY = "bigram_similarity"
 W2V_COSINE_SIMILARITY = "w2v_cosine_similarity"
-CATEGORY = "category"
-IS_ACCEPTED = "is_accepted"
-SCORE = "score"
-USER_ID = "user_id"
-TAGS = "tags"
-VIEW_COUNT = "view_count"
+
+NO_OF_COMMENTS = "no_of_comments"
+NO_OF_ANSWERS = "no_of_answers"
+TOTAL_ANSWER_UPVOTES = "total_answer_upvotes"
+BEST_ANSWER_UPVOTES = "best_answer_upvotes"
+HAS_ACCEPTED_ANSWER = "has_accepted_answer"
+
 
 def kill_program(output_file, iteration_num):
-    print("Program stopped at {} file, {} iteration".format(output_file, iteration_num))
+    print("Program stopped at {} file, {} iteration".format(
+        output_file, iteration_num))
     exit()
-
-
-def set_default_row_values(row):
-    specific_keys = [CATEGORY, IS_ACCEPTED, SCORE, USER_ID, TAGS, VIEW_COUNT, RELEVANCE]
-
-    for key in specific_keys:
-        if row[key] is None:
-            row[key] = "n/a"
 
 
 class SigTermKiller:
@@ -59,134 +58,6 @@ class Questions(object):
         self.iteration = 0
         self.progress = 0.0
 
-    def related_question(self, original_question, model):
-        original_question_body = original_question.findtext("OrgQBody")
-        related_question = original_question.find(".//RelQuestion")
-        related_question_body = related_question.findtext("RelQBody")
-
-        row = {}
-        row[ORIGINAL_QUESTION_ID] = original_question.get('ORGQ_ID')
-        row[TYPE] = 'RELQ'
-
-        row[RELATED_ID] = related_question.get('RELQ_ID')
-        row[CATEGORY] = related_question.get('RELQ_CATEGORY')
-        row[SCORE] = related_question.get('RELQ_SCORE')
-        row[TAGS] = related_question.get('RELQ_TAGS')
-        row[VIEW_COUNT] = related_question.get('RELQ_VIEWCOUNT')
-        row[USER_ID] = related_question.get('RELQ_USERID')
-        row[RELEVANCE] = related_question.get('RELQ_RELEVANCE2ORGQ')
-
-        row[JACCARD_DISTANCE] = round(basic_stats.jaccard_distance(
-            original_question_body, related_question_body), 3)
-        row[LENGTH_DIFFERENCE] = basic_stats.length_difference(
-            original_question_body, related_question_body)
-        row[COSINE_SIMILARITY] = round(basic_stats.cosine_similarity(
-            model, original_question_body, related_question_body), 3)
-        row[BIGRAM_SIMILARITY] = round(basic_stats.ngram_similarity(
-            original_question_body, related_question_body, n=2), 3)
-
-        if self.w2v_model:
-            related_question_vector = word2vec_utils.sentence_vectors_mean(
-                word2vec_utils.sentence2vectors(related_question_body,
-                                                self.w2v_model,
-                                                to_lower_case=True,
-                                                verbose=False))
-            original_question_vector = word2vec_utils.sentence_vectors_mean(
-                word2vec_utils.sentence2vectors(original_question_body,
-                                                self.w2v_model,
-                                                to_lower_case=True,
-                                                verbose=False))
-
-            row[W2V_COSINE_SIMILARITY] = word2vec_utils.vectors_cosine_similarity(original_question_vector,
-                                                                                  related_question_vector)
-
-        return row
-
-    def related_answers(self, original_question, model):
-        original_question_body = original_question.findtext("OrgQBody")
-        related_answers = original_question.findall(".//RelAnswer")
-
-        answers = []
-        for related_answer in related_answers:
-            related_answer_body = related_answer.findtext("RelAText")
-
-            row = {}
-            row[ORIGINAL_QUESTION_ID] = original_question.get('ORGQ_ID')
-            row[TYPE] = 'RELA'
-
-            row[RELATED_ID] = related_answer.get('RELA_ID')
-            row[SCORE] = related_answer.get('RELA_SCORE')
-            row[IS_ACCEPTED] = related_answer.get('RELA_ACCEPTED')
-            row[USER_ID] = related_answer.get('RELA_USERID')
-
-            row[JACCARD_DISTANCE] = round(basic_stats.jaccard_distance(
-                original_question_body, related_answer_body), 3)
-            row[LENGTH_DIFFERENCE] = basic_stats.length_difference(
-                original_question_body, related_answer_body)
-            row[COSINE_SIMILARITY] = round(basic_stats.cosine_similarity(
-                model, original_question_body, related_answer_body), 3)
-            row[BIGRAM_SIMILARITY] = round(basic_stats.ngram_similarity(
-                original_question_body, related_answer_body, n=2), 3)
-
-            if self.w2v_model:
-                related_answer_vector = word2vec_utils.sentence_vectors_mean(
-                    word2vec_utils.sentence2vectors(related_answer_body,
-                                                    self.w2v_model,
-                                                    to_lower_case=True,
-                                                    verbose=False))
-                original_question_vector = word2vec_utils.sentence_vectors_mean(
-                    word2vec_utils.sentence2vectors(original_question_body,
-                                                    self.w2v_model,
-                                                    to_lower_case=True,
-                                                    verbose=False))
-
-                row[W2V_COSINE_SIMILARITY] = word2vec_utils.vectors_cosine_similarity(original_question_vector,
-                                                                                    related_answer_vector)
-
-            answers.append(row)
-
-        return answers
-
-    def related_comments(self, original_question, model):
-        original_question_body = original_question.findtext("OrgQBody")
-        related_comments = original_question.findall(".//RelComment")
-
-        comments = []
-        for related_comment in related_comments:
-            related_comment_body = related_comment.findtext("RelCText")
-
-            row = {}
-            row[ORIGINAL_QUESTION_ID] = original_question.get('ORGQ_ID')
-            row[TYPE] = 'RELC'
-            row[RELATED_ID] = related_comment.get('RELC_ID')
-            row[JACCARD_DISTANCE] = round(basic_stats.jaccard_distance(
-                original_question_body, related_comment_body), 3)
-            row[LENGTH_DIFFERENCE] = basic_stats.length_difference(
-                original_question_body, related_comment_body)
-            row[COSINE_SIMILARITY] = round(basic_stats.cosine_similarity(
-                model, original_question_body, related_comment_body), 3)
-            row[BIGRAM_SIMILARITY] = round(basic_stats.ngram_similarity(
-                original_question_body, related_comment_body, n=2), 3)
-
-            if self.w2v_model:
-                related_comment_vector = word2vec_utils.sentence_vectors_mean(
-                    word2vec_utils.sentence2vectors(related_comment_body,
-                                                    self.w2v_model,
-                                                    to_lower_case=True,
-                                                    verbose=False))
-                original_question_vector = word2vec_utils.sentence_vectors_mean(
-                    word2vec_utils.sentence2vectors(original_question_body,
-                                                    self.w2v_model,
-                                                    to_lower_case=True,
-                                                    verbose=False))
-
-                row[W2V_COSINE_SIMILARITY] = word2vec_utils.vectors_cosine_similarity(original_question_vector,
-                                                                                      related_comment_vector)
-
-            comments.append(row)
-
-        return comments
-
     def iterator_cleanup(self, element):
         element.clear()
 
@@ -196,47 +67,99 @@ class Questions(object):
     def __iter__(self):
         model = spacy.load("en")
         file_size = path.getsize(self.xml_path)
+
         with open(self.xml_path, 'rb') as fp:
             for i, (event, original_question) in enumerate(etree.iterparse(fp, tag="OrgQuestion")):
+                self.iteration = i
                 if self.skip > i:
                     self.iterator_cleanup(original_question)
                     continue
 
-                self.iteration = i
+                original_question_body = original_question.findtext("OrgQBody")
+                related_question = original_question.find(".//RelQuestion")
+                related_question_body = related_question.findtext('RelQBody')
+                comments = original_question.findall('.//RelComment')
+                answers = original_question.findall('.//RelAnswer')
 
-                # There's always one related question
-                yield self.related_question(original_question, model)
+                row = {}
+                row[ORIGINAL_QUESTION_ID] = original_question.get('ORGQ_ID')
+                row[RELATED_QUESTION_ID] = related_question.get('RELQ_ID')
+                row[CATEGORY] = related_question.get('RELQ_CATEGORY')
+                row[SCORE] = related_question.get('RELQ_SCORE')
+                row[TAGS] = related_question.get('RELQ_TAGS').replace(',', ';')
+                row[VIEW_COUNT] = related_question.get('RELQ_VIEWCOUNT')
+                row[USER_ID] = related_question.get('RELQ_USERID')
+                row[RELEVANCE] = related_question.get('RELQ_RELEVANCE2ORGQ')
+                row[NO_OF_COMMENTS] = len(comments)
+                row[NO_OF_ANSWERS] = len(answers)
 
-                for answer_row in self.related_answers(original_question, model):
-                    yield answer_row
+                answers_scores = [int(answer.get('RELA_SCORE'))
+                                  for answer in answers]
+                row[TOTAL_ANSWER_UPVOTES] = sum(answers_scores)
+                row[BEST_ANSWER_UPVOTES] = max(answers_scores, default=0)
+                row[HAS_ACCEPTED_ANSWER] = any([bool(answer.get('RELA_ACCEPTED')) for answer in answers])
+
+                row[JACCARD_DISTANCE] = round(basic_stats.jaccard_distance(
+                    original_question_body, related_question_body), 3)
+                row[LENGTH_DIFFERENCE] = basic_stats.length_difference(
+                    original_question_body, related_question_body)
+                row[COSINE_SIMILARITY] = round(basic_stats.cosine_similarity(
+                    model, original_question_body, related_question_body), 3)
+                row[BIGRAM_SIMILARITY] = round(basic_stats.ngram_similarity(
+                    original_question_body, related_question_body, n=2), 3)
+
+                related_question_vector = word2vec_utils.sentence_vectors_mean(
+                    word2vec_utils.sentence2vectors(related_question_body,
+                                                    self.w2v_model,
+                                                    to_lower_case=True,
+                                                    verbose=False))
+                original_question_vector = word2vec_utils.sentence_vectors_mean(
+                    word2vec_utils.sentence2vectors(original_question_body,
+                                                    self.w2v_model,
+                                                    to_lower_case=True,
+                                                    verbose=False))
+                row[W2V_COSINE_SIMILARITY] = word2vec_utils.vectors_cosine_similarity(original_question_vector,
+                                                                                      related_question_vector)
 
                 if self.verbose:
                     self.progress = float(fp.tell() / file_size) * 100.0
                     print("Iteration = {}, File progress = {:2.2f}%".format(
                         i, self.progress), end="\r")
 
+                yield row
                 self.iterator_cleanup(original_question)
 
 
-def stats(src, dest='Duplicate-Question-stats.csv', model_path='../word2vec_model/subtask-e-word-model', append=False, skip=0, verbose=False):
+def stats(src, dest='Duplicate-Question-stats.csv', model_path='../word2vec_model/subtask-e-word-model', skip=0, verbose=False):
     if not path.isfile(src):
         raise FileNotFoundError("Source file not found at {}".format(src))
 
     if not path.isfile(model_path):
-        raise FileNotFoundError("Model file not found at {}".format(model_path))
+        raise FileNotFoundError(
+            "Model file not found at {}".format(model_path))
 
     sig_term_killer = SigTermKiller()
 
+
+    if verbose:
+        print("Loading word2vec model located at {}...".format(model_path))
+
     word2vec_model = gensim.models.Word2Vec.load(model_path)
 
+    if verbose:
+        print("word2vec model loaded.")
+
+
+    append = skip > 0
     file_mode = 'a' if append else 'w'
     with open(dest, file_mode) as csvfile:
-        fieldnames = [ORIGINAL_QUESTION_ID, TYPE, RELATED_ID, JACCARD_DISTANCE,
-                      LENGTH_DIFFERENCE, COSINE_SIMILARITY, BIGRAM_SIMILARITY,
-                      W2V_COSINE_SIMILARITY, CATEGORY, IS_ACCEPTED, SCORE, USER_ID,
-                      TAGS, VIEW_COUNT, RELEVANCE]
+        fieldnames = [ORIGINAL_QUESTION_ID, RELATED_QUESTION_ID, CATEGORY, TAGS, SCORE, VIEW_COUNT,
+                      USER_ID, HAS_ACCEPTED_ANSWER, NO_OF_COMMENTS, NO_OF_ANSWERS, TOTAL_ANSWER_UPVOTES,
+                      BEST_ANSWER_UPVOTES, JACCARD_DISTANCE, LENGTH_DIFFERENCE, COSINE_SIMILARITY,
+                      BIGRAM_SIMILARITY, W2V_COSINE_SIMILARITY, RELEVANCE]
+
         writer = csv.DictWriter(
-            csvfile, fieldnames=fieldnames, delimiter=';', lineterminator='\n')
+            csvfile, fieldnames=fieldnames, lineterminator='\n')
 
         if not append:
             writer.writeheader()
@@ -245,17 +168,20 @@ def stats(src, dest='Duplicate-Question-stats.csv', model_path='../word2vec_mode
             src, word2vec_model, append=append, skip=skip, verbose=verbose)
         try:
             for row in questions_iterator:
-                set_default_row_values(row)
                 writer.writerow(row)
 
                 if sig_term_killer.kill:
                     kill_program(dest, questions_iterator.iteration)
         except Exception as error:
             print(error)
+
             # Save last iteration number and file progress in .txt file
-            with open('last-stats.txt', 'w') as fp:
-                fp.write('LastIteration={}'.format(questions_iterator.iteration))
-                fp.write('Progress={}'.format(questions_iterator.progress))
+            with open('last-stats.txt', 'w') as stats_fp:
+                stats_fp.write('LastIteration={}'.format(
+                    questions_iterator.iteration))
+                stats_fp.write('Progress={}'.format(
+                    questions_iterator.progress))
+
 
 if __name__ == '__main__':
     import argparse
@@ -275,6 +201,5 @@ if __name__ == '__main__':
     stats(args.data,
           dest=args.output,
           model_path=args.model,
-          append=args.skip > 0,
           skip=args.skip,
           verbose=args.verbose)
